f8e6a10eabf3a2617d502c3107973aeed0053144
Adam Ćwian <adam.cwian@comarch.pl>
2024-02-04 20:03:05 +0100

    Add integration-tests

--- outfit-planner-system/integration-tests/src/main/java/com/outfit/planner/system/OutfitPlannerTestRunner.java
+++ outfit-planner-system/integration-tests/src/main/java/com/outfit/planner/system/OutfitPlannerTestRunner.java
@@ -1,6 +1,7 @@
 package com.outfit.planner.system;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
@@ -13,15 +14,19 @@ public class OutfitPlannerTestRunner {
 
     public static void main(String[] args) {
         String url = (args.length > 0) ? args[0] : "https://dashboard.outfitplanner.com";
+        log.info("Hello from here arhs {}", url);
+        System.out.println("kekekeke helo");
 
-        try {
+//        try {
             test1(url);
-        }catch (Exception e){
-            System.exit(1);
-        }
+//        }catch (Exception e){
+//            System.exit(1);
+//        }
     }
 
     private static void test1(String url) {
+        Selenide.sleep(5000);
+
         open(url);
 
         SelenideElement header = $(By.cssSelector(".shadow.dashboard-view__header span"));
