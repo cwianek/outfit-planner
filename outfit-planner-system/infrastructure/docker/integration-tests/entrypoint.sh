3050bf9b7533354fd89e3912b6f61bb6e3130d5e
Adam Ćwian <adam.cwian@comarch.pl>
2024-02-06 18:39:58 +0100

    Integration tests

--- outfit-planner-system/infrastructure/docker/integration-tests/entrypoint.sh
+++ outfit-planner-system/infrastructure/docker/integration-tests/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 APP_URL="${APP_URL:-default-website-url}"
+REMOTE_URL="${REMOTE_URL:-http://localhost:4444/wd/hub}"
 
-java -jar integration-tests.jar $APP_URL
+java -jar -Dselenide.remote=$REMOTE_URL integration-tests.jar $APP_URL
