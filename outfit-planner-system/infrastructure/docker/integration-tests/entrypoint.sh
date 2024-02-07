16143e62f4ff2b82d78fbb6a00c3cc71a9ecef5d
Adam Ćwian <adam.cwian@comarch.pl>
2024-02-07 19:45:02 +0100

    Workflow action kubernetes tests

--- outfit-planner-system/infrastructure/docker/integration-tests/entrypoint.sh
+++ outfit-planner-system/infrastructure/docker/integration-tests/entrypoint.sh
@@ -5,6 +5,17 @@ REMOTE_URL="${REMOTE_URL:-http://localhost:4444/wd/hub}"
 
 mkdir -p /app/test-results
 
-LOG_FILE="/app/test-results/test_logs.txt"
+STATUS_FILE="/app/test_status.txt"
 
-java -jar -Dselenide.remote=$REMOTE_URL integration-tests.jar $APP_URL 2>&1 | tee "$LOG_FILE"
+java -jar -Dselenide.remote=$REMOTE_URL integration-tests.jar $APP_URL
+JAVA_EXIT_CODE=$?
+
+if [ $JAVA_EXIT_CODE -eq 0 ]; then
+  TEST_STATUS="success"
+else
+  TEST_STATUS="failure"
+fi
+
+echo $TEST_STATUS > "$STATUS_FILE"
+
+tail -f /dev/null
